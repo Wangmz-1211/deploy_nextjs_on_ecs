1a43b99e31c4c8b314a40817e597b5751440925c
wangmz <w2639764449@hotmail.com>
2023-12-16 14:51:15 +0800

    🗑️ Workflow test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,6 +4,7 @@ const Page = () => {
             <h1>Test</h1>
             <p>This page is only for test use.</p>
             <p>Update by aws-cli force update ecs service</p>
+            <p>Test CI/CD</p>
         </div>
     )
 }
